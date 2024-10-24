9cd54821e316265e3402506c24bf02b85ecc00f9
edgmsh <edgmsh@gmail.com>
2024-10-24 11:47:15 -0400

    first commit

--- play.js
+++ play.js
@@ -1,25 +1,5 @@
+const { connect } = require('./client.js');
 const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
-    console.log("connection is established");
-  });
-
-  conn.on("data", (data) => {
-    console.log("the server says: ", data);
-  });
-  
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
