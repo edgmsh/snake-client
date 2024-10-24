ca05570bb429d73296faaec2daf20bc54a849928
edgmsh <edgmsh@gmail.com>
2024-10-24 17:56:35 -0400

    next v

--- input.js
+++ input.js
@@ -1,18 +1,30 @@
 let connection;
+const movementCommands = {
+  'w': 'Move up',
+  'a': 'Move left',
+  's': 'Move down',
+  'd': 'Move right',
+  'q': 'Say: Done for 2day',
+  'h': 'Say: Hi everyone',
+  'i': 'Say: Im the best'
+};
+
 
 const handleUserInput = function(key) {
   if (key === "\u0003") {
     process.exit();
   } else if (key === "w") {
-    connection.write('Move: up');
+    connection.write(movementCommands['w']);
   } else if (key === "a") {
-    connection.write('Move: left');
+    connection.write(movementCommands['a']);
   } else if (key === "s") {
-    connection.write('Move: down');
+    connection.write(movementCommands['s']);
   } else if (key === "d") {
-    connection.write('Move: right');
-  } else if (key === "e") {
-    connection.write("Say: :)")
+    connection.write(movementCommands['d']);
+  } else if (key === "i") {
+    connection.write(movementCommands['i'])
+  } else if (key === "q") {
+    connection.write(movementCommands['q'])
   }
 };
 
