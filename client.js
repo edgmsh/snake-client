ae53148be04a41bb7690b0a22769c84307ca1433
edgmsh <edgmsh@gmail.com>
2024-10-24 12:12:37 -0400

    first commit

--- client.js
+++ client.js
@@ -11,7 +11,7 @@ const connect = function () {
   conn.setEncoding("utf8");
 
   conn.on("connect", () => {
-    console.log("connection is established");
+    console.log("Successfully connected to game server");
   });
 
   conn.on("data", (data) => {
